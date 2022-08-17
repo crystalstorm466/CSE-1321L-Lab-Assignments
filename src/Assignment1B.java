e2354c09ad61385f0cf54a3eda2e460aa932c677
David Holland <78986447+crystalstorm466@users.noreply.github.com>
2022-08-17 17:09:21 -0400

    finished for class will work on later

---
+++ src/Assignment1B.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Assignment1B {
+
+    public static void main(String args[])
+    {
+        float labGrade;
+        float assignmentGrade;
+        float midtermGrade;
+        float finalGrade;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your average lab grade: ");
+        labGrade = scan.nextFloat();
+        scan = new Scanner(System.in);
+        System.out.print("Enter your average assignment grade: ");
+        assignmentGrade = scan.nextFloat();
+        scan = new Scanner(System.in);
+        System.out.print("Enter your average midterm exam grade: ");
+        midtermGrade = scan.nextFloat();
+        scan = new Scanner(System.in);
+        System.out.print("Enter your final exam grade: ");
+        finalGrade = scan.nextFloat();
+
+        float weightedLab;
+        float weightedAssignment;
+
+    }
+}
