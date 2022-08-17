83ad15af1d5fb473c9604b011b2a9dae0582b6b3
David Holland <78986447+crystalstorm466@users.noreply.github.com>
2022-08-17 16:50:02 -0400

    Add files via upload

--- src/Assignment1A.java
+++ src/Assignment1A.java
@@ -16,7 +16,13 @@ public class Assignment1A {
         height = scan.nextDouble();
         scan = new Scanner (System.in);
         System.out.print("Enter the resolution (PPI): ");
+        resolution = scan.nextDouble();
 
+        double widthinches;
+        double heightinches;
+        widthinches = width / resolution;
+        heightinches = height / resolution;
+        System.out.println("At " + resolution + " PPI, the image is " + widthinches + " wide by " + heightinches + " high.");
 
     }
 }
