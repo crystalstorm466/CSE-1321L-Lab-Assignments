b589898ef1f843bb3062da1648d8197a13235a3d
David Holland <78986447+crystalstorm466@users.noreply.github.com>
2022-08-17 16:41:31 -0400

    Assignment1A

---
+++ src/Assignment1A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Assignment1A {
+
+    public static void main(String args[])
+    {
+        double width;
+        double height;
+        double resolution;
+
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Enter the width (in pixels: ");
+        width = scan.nextDouble();
+        scan = new Scanner (System.in);
+
+        System.out.print("Enter the height (in pixels): ");
+        height = scan.nextDouble();
+        scan = new Scanner (System.in);
+        System.out.print("Enter the resolution (PPI): ");
+
+
+    }
+}
