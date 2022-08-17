022dd4224f8ce7c93162ffa166a76da451ea784f
David Holland <78986447+crystalstorm466@users.noreply.github.com>
2022-08-17 17:01:15 -0400

    Finished in class; Will do more later

--- src/Assignment1A.java
+++ src/Assignment1A.java
@@ -3,26 +3,26 @@ public class Assignment1A {
 
     public static void main(String args[])
     {
-        double width;
-        double height;
-        double resolution;
+        float width;
+        float height;
+        float resolution;
 
         Scanner scan = new Scanner (System.in);
-        System.out.print("Enter the width (in pixels: ");
-        width = scan.nextDouble();
+        System.out.print("Enter the width (in pixels): ");
+        width = scan.nextFloat();
         scan = new Scanner (System.in);
 
         System.out.print("Enter the height (in pixels): ");
-        height = scan.nextDouble();
+        height = scan.nextFloat();
         scan = new Scanner (System.in);
         System.out.print("Enter the resolution (PPI): ");
-        resolution = scan.nextDouble();
+        resolution = scan.nextFloat();
 
-        double widthinches;
-        double heightinches;
+        float widthinches;
+        float heightinches;
         widthinches = width / resolution;
         heightinches = height / resolution;
-        System.out.println("At " + resolution + " PPI, the image is " + widthinches + " wide by " + heightinches + " high.");
+        System.out.println("At " + resolution + " PPI, the image is " + widthinches + " wide by " + heightinches + "'' high.");
 
     }
 }
