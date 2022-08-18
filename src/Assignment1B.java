ee112006e22f84bf5fd120caba30719108891c17
David Holland <DcHolland_89@msn.com>
2022-08-17 21:57:20 -0400

    Finished Assignment1B.java this should work now.

--- src/Assignment1B.java
+++ src/Assignment1B.java
@@ -7,7 +7,7 @@ public class Assignment1B {
         float assignmentGrade;
         float midtermGrade;
         float finalGrade;
-
+        System.out.println("[CSE 1321L Final Grade Calculator]");
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your average lab grade: ");
         labGrade = scan.nextFloat();
@@ -21,8 +21,26 @@ public class Assignment1B {
         System.out.print("Enter your final exam grade: ");
         finalGrade = scan.nextFloat();
 
-        float weightedLab;
-        float weightedAssignment;
+        double weightedLab;
+        double weightedAssignment;
+        double weightedMidterm;
+        double weightedFinalExam;
+
+        weightedLab = labGrade * 0.40;
+        weightedAssignment = assignmentGrade * 0.10;
+        weightedMidterm = midtermGrade * 0.20;
+        weightedFinalExam = finalGrade * 0.30;
+        System.out.println("Your weighted lab average is " + weightedLab + ".");
+        System.out.println("Your weighted assignment average is " + weightedAssignment + ".");
+        System.out.println("Your weighted midterm average is " + weightedMidterm + ".");
+        System.out.println("Your weighted final exam average is " + weightedFinalExam + ".");
+        System.out.println("Adding Numbers...");
+        double totalGrade;
+        totalGrade = weightedLab + weightedAssignment + weightedMidterm + weightedFinalExam;
+        System.out.println("Your Final Grade in CSE 1321L is " + totalGrade);
+
+
+
 
     }
 }
