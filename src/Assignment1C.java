ea9ea0da759dbafccc0400806ea6d90e32835b59
David Holland <DcHolland_89@msn.com>
2022-08-19 12:24:54 -0400

    made it a bit more readable

--- src/Assignment1C.java
+++ src/Assignment1C.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class Assignment1C {
     public static void main(String args[]) {
-        char input;
+            char userChar;
+//        The variable was originally input, but I changed it to userChar to make it more readable
         System.out.println("[And the Next Letter is]");
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter a letter: ");
-        input = scan.next().charAt(0);
-        char char2 = (char) (input + 1);
+        userChar = scan.next().charAt(0);
+        char char2 = (char) (userChar + 1);
         char char3 = (char) (char2 + 1);
         char char4 = (char) (char3 + 1);
-        System.out.print("The next letters after " + input + " are " + char2 + ", " + char3 + ", and " + char4 + "!");
+        System.out.print("The next letters after " + userChar + " are " + char2 + ", " + char3 + ", and " + char4 + "!");
     }
 
 }
