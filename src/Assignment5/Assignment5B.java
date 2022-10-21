814bf9f366ccef17f384aeb6fc7c438ddc1b46fc
crystalstorm466 <DcHolland_89@msn.com>
2022-10-21 14:14:43 -0400

    5b took me way to long to make the array

--- src/Assignment5/Assignment5B.java
+++ src/Assignment5/Assignment5B.java
@@ -7,34 +7,109 @@ public class Assignment5B {
 
         System.out.println("What month were you born in? ");
         int month = scan.nextInt();
-        if (month >= 13) {
+        if (month > 13 || month <= 1) {
             System.out.println("That is not a valid month...\n");
             System.exit(0);
         }
+        System.out.println("What day were you born in? ");
         int day = scan.nextInt();
-        if (month >= 13) {
+        if (day > 32 || day <= 1) {
             System.out.println("That is not a valid day...\n");
             System.exit(0);
         }
 
-
-        String [] [] signTable = new String [12] [31];
-        for (int fixedMonth = 1; fixedMonth < 12; month++) {
-            for (int fixedDay = 1; fixedDay < 21; fixedDay++) {
-                if ((fixedMonth == 12 || fixedMonth == 1)  & fixedDay >= 22) {
-                    signTable[12] [fixedDay] = "Capricornus";
-                } else (fixedMonth == 1 || fixedMonth == 2 & fixedDay >= 20 ) {
-                    signTable[]
+        String[][] signTable = new String[13][32];
+        for (int permMonth = 1; permMonth < 13; permMonth++) {
+            for (int permDay = 1; permDay < 32; permDay++) {
+                switch (permMonth) {
+                    case 1:
+                        if (permDay < 20) {
+                            signTable[permMonth][permDay] = "Capricorn";
+                        } else {
+                            signTable[permMonth][permDay] = "Aquarius";
+                        }
+                        break;
+                    case 2:
+                        if (permDay < 18) {
+                            signTable[permMonth][permDay] = "Aquarius";
+                        } else {
+                            signTable[permMonth][permDay] = "Pisces";
+                        }
+                        break;
+                    case 3:
+                        if (permDay < 21) {
+                            signTable[permMonth][permDay] = "Pisces";
+                        } else {
+                            signTable[permMonth][permDay] = "Aries";
+                        }
+                        break;
+                    case 4:
+                        if (permDay < 20) {
+                            signTable[permMonth][permDay] = "Aries";
+                        } else {
+                            signTable[permMonth][permDay] = "Taurus";
+                        }
+                        break;
+                    case 5:
+                        if (permDay < 21) {
+                            signTable[permMonth][permDay] = "Taurus";
+                        } else {
+                            signTable[permMonth][permDay] = "Gemini";
+                        }
+                        break;
+                    case 6:
+                        if (permDay < 21) {
+                            signTable[permMonth][permDay] = "Gemini";
+                        } else {
+                            signTable[permMonth][permDay] = "Cancer";
+                        }
+                        break;
+                    case 7:
+                        if (permDay < 23) {
+                            signTable[permMonth][permDay] = "Cancer";
+                        } else {
+                            signTable[permMonth][permDay] = "Leo";
+                        }
+                        break;
+                    case 8:
+                        if (permDay < 23) {
+                            signTable[permMonth][permDay] = "Leo";
+                        } else {
+                            signTable[permMonth][permDay] = "Virgo";
+                        }
+                        break;
+                    case 9:
+                        if (permDay < 23) {
+                            signTable[permMonth][permDay] = "Virgo";
+                        } else {
+                            signTable[permMonth][permDay] = "Libra";
+                        }
+                        break;
+                    case 10:
+                        if (permDay < 23) {
+                            signTable[permMonth][permDay] = "Libra";
+                        } else {
+                            signTable[permMonth][permDay] = "Scorpio";
+                        }
+                        break;
+                    case 11:
+                        if (permDay < 22) {
+                            signTable[permMonth][permDay] = "Scorpio";
+                        } else {
+                            signTable[permMonth][permDay] = "Sagittarius";
+                        }
+                        break;
+                    case 12:
+                        if (permDay < 22) {
+                            signTable[permMonth][permDay] = "Sagittarius";
+                        } else {
+                            signTable[permMonth][permDay] = "Capricorn";
+                        } //this took forever to type out
+                        break;
                 }
             }
         }
-
-        String yourSign = signTable[month] [day];
-        System.out.println("Your sign is " + yourSign);
-
-
-
-
+        System.out.println("Your sign is " + signTable[month][day] + "!");
     }
 }
 
