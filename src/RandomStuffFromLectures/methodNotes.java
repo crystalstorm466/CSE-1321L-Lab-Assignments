fe27b86df32708eeda6c7f1c3588a9bc0f3272bd
David Holland <DcHolland_89@msn.com>
2022-11-30 10:52:53 -0500

    updated

--- src/RandomStuffFromLectures/methodNotes.java
+++ src/RandomStuffFromLectures/methodNotes.java
@@ -2,7 +2,6 @@ package RandomStuffFromLectures;
 import java.util.*;
 public class methodNotes {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
         String userInput = testModule();
 
